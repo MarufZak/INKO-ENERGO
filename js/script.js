83f70215710508677b0f72123ff22d89eee43f14
Maruf <maruf.zakirov.20042004@gmail.com>
2021-05-22 20:27:24 +0500

    everyDone

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let swiper = new Swiper('.swiper',{
+    wrapperClass: "swiper__wrapper",
+    slideClass: "swiper-slide",
+    direction: "vertical",
+    mousewheel: {
+        sensitivity: 1
+    },
+    pagination: {
+        el: ".swiper__pagination",
+        type: 'bullets',
+        bulletClass: "swiper__bullet",
+        bulletActiveClass: "swiper__bullet-active"
+    },
+    speed: 800,
+
+})
+
+$('.sidebar__burger').click(function () {  
+    $('.sidebar__nav').toggleClass('active');
+    $('.swiper-slide__first--body').toggleClass('active');
+    $('.sidebar__burger').toggleClass('active');
+    $('.header__link--first').toggleClass('active');
+    $('.header__link--last').toggleClass('active');
+})

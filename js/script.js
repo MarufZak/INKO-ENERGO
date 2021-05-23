faa7d4a77d0f8dcab70380335880c4eb9666fbac
Maruf <maruf.zakirov.20042004@gmail.com>
2021-05-23 13:12:19 +0500

    mainwork

--- js/script.js
+++ js/script.js
@@ -26,3 +26,57 @@ $('.sidebar__burger').click(function () {
 $('.sidebar__link').click(function (e) {  
     e.preventDefault()
 })
+
+
+if ($(window).width() > 535) {
+    $('.header__link--last').click(function (e) {  
+        e.preventDefault()
+        $('.swiper-slide__write--body').addClass('hidden')
+        $('.swiper-slide__call--body').removeClass('hidden');
+        $('.swiper-slide__first--body').addClass('hidden');
+        $('.swiper-slide__call--cancel').click(function () {  
+            $('.swiper-slide__call--body').addClass('hidden');
+            $('.swiper-slide__first--body').removeClass('hidden');
+        })
+    })
+    
+    $('.header__link--first').click(function (e) {  
+        e.preventDefault()
+        $('.swiper-slide__first--body').addClass('hidden');
+        $('.swiper-slide__call--body').addClass('hidden');
+        $('.swiper-slide__write--body').removeClass('hidden');
+        $('.swiper-slide__write--cancel').click(function () {  
+            $('.swiper-slide__first--body').removeClass('hidden');
+            $('.swiper-slide__write--body').addClass('hidden');  
+        })
+        
+    })
+} else {
+    $('.header__link--last').click(function (e) {  
+        e.preventDefault()
+        $('.sidebar__nav').removeClass('active')
+        $('.sidebar__burger').removeClass('active')
+        $('.swiper-slide__first--body').removeClass('active')
+        $('.swiper-slide__call--body').removeClass('hidden');
+        $('.header__link--last').removeClass('active')
+        $('.header__link--first').removeClass('active')
+        $('.swiper-slide__call--cancel').click(function () {  
+            $('.swiper-slide__call--body').addClass('hidden');
+            $('.swiper-slide__first--body').removeClass('active');
+        })
+    })
+
+    $('.header__link--first').click(function (e) {  
+        e.preventDefault()
+        $('.sidebar__nav').removeClass('active')
+        $('.sidebar__burger').removeClass('active')
+        $('.swiper-slide__last--body').removeClass('active')
+        $('.swiper-slide__call--body').removeClass('hidden');
+        $('.header__link--first').removeClass('active')
+        $('.header__link--last').removeClass('active')
+        $('.swiper-slide__last--cancel').click(function () {  
+            $('.swiper-slide__call--body').addClass('hidden');
+            $('.swiper-slide__first--body').removeClass('active');
+        })
+    })
+}
